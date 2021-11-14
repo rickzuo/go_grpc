9ebf6ff6247df2d6d6d51e8b23cce7f1519e6687
zuoak <zuoak@jiguang.cn>
2021-11-14 10:13:26 +0800

    fix limit and time sleep 1s

--- tools/goroutine-cancel/main2.go
+++ tools/goroutine-cancel/main2.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
+	"time"
 )
+
 var Counter = 0
 
-func Worker2(id int, cancel chan bool, wg *sync.WaitGroup, limit chan bool) {
+func Worker2(id int, cancel chan bool, wg *sync.WaitGroup, limit chan struct{}) {
 	defer wg.Done()
 
+	Counter++
+	time.Sleep(time.Second)
+	fmt.Println("goid is:", id, "Counter:", Counter)
+	// 注意这个位置
 	<-limit
-	Counter ++
-
-	fmt.Println("goid is:", id,"Counter:",Counter)
-	for {
-		select {
+	select {
 		case <-cancel:
 			fmt.Println("quit goid:", id)
 			return
 		default:
-			//fmt.Println("working goid:", id)
-		}
+		//fmt.Println("working goid:", id)
 	}
 }
 
@@ -29,19 +29,18 @@ func main() {
 
 	cancel := make(chan bool)
 	// 控制并发数为10
-	limit := make(chan bool, 10)
+	limit := make(chan struct{}, 3)
 	var wg sync.WaitGroup
 
 	for i := 0; i < 100; i++ {
 		wg.Add(1)
-		limit <- true
-		fmt.Println("limit len",len(limit))
+		limit <- struct{}{}
 		go Worker2(i, cancel, &wg, limit)
-		fmt.Println("===========" + strconv.Itoa(i) + "=============")
+		//fmt.Println("===========" + strconv.Itoa(i) + "=============")
 	}
 	//我们通过close来关闭cancel管道向多个Goroutine广播退出的指令
 	close(cancel)
-	fmt.Println("done:",Counter)
+	fmt.Println("done:", Counter)
 
 	wg.Wait()
 
